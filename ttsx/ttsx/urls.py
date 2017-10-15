89d4960b1467cf0a3d466a0a01706f9ec0c63bef
cyuq <fengtianzq@163.com>
2017-10-15 14:54:22 +0800

    配置用户url

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^user/',include('df_user.urls')),
+    url(r'^adduser/',include('df_user.urls')),
+    ]
